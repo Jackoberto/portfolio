e246caa5bea5a34e57adb10bbbfdca952711f2c2
Jack Karlsson <jackoberto01@gmail.com>
2026-03-10 02:28:56 +0100

    Add robots.txt and generate sitemap.xml with eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("js/theme.js");
   eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("robots.txt");
 
   return {
     dir: {
